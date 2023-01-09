0d0b729fe6cc667f980944ae3f5b17433e3c8250
Blake Stephens <blak7@pdx.edu>
2023-01-09 11:42:16 -0800

    got input to clear after enter pressed

---
+++ src/commands.js
@@ -0,0 +1,7 @@
+export const add = () => { }
+export const edit = () => { }
+export const get = () => { }
+export const help = () => { }
+export const list = () => { }
+export const last = () => { }
+export const search = () => { }
---
+++ src/rightcol.js
@@ -0,0 +1,8 @@
+// import { Database } from 'sqlite3';
+// const db = new Database('db.sqlite');
+// const test = db.get("SELECT RANDOM() % 100 as result")
+export function Tags() {
+  return (
+    <div id="tags-list"></div>
+  )
+}
